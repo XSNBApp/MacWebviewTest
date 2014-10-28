68a08d2edf32c8fc8fce546b5021dc92603f7f69
xsnber <xxing@xxingdeMacBook-Pro.local>
2014-10-28 15:52:08 +0800

    test

---
+++ MacWebView/WebView/resources/AppData/SNBWebview/SNBWebview/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  SNBWebview
+//
+//  Created by xxing on 14/10/28.
+//  Copyright (c) 2014年 xxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SNBMainNavViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong,nonatomic) SNBMainNavViewController* mainNavViewCtrl;
+
+@end
+
---
+++ MacWebView/WebView/resources/AppData/SNBWebview/SNBWebview/UI/MainNav/SNBMainNavViewController.h
@@ -0,0 +1,13 @@
+//
+//  SNBMainNavViewController.h
+//  SNBWebview
+//
+//  Created by xxing on 14/10/28.
+//  Copyright (c) 2014年 xxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNBMainNavViewController : UIViewController
+
+@end
